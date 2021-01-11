d0b94c7ba42aad88639db428fecf12fa7b4d94fd
Geremias Corrêa <gere.c@hotmail.com>
2021-01-11 08:54:45 -0300

    Changing the main archive to web anottations.

--- src/main/java/PSProjects/RentalVehiclesTest/RentalVehiclesTestApplication.java
+++ src/main/java/PSProjects/RentalVehiclesTest/RentalVehiclesTestApplication.java
@@ -2,10 +2,25 @@ package PSProjects.RentalVehiclesTest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 @SpringBootApplication
 public class RentalVehiclesTestApplication {
 
+	@RequestMapping("/")
+    public String home(){
+        return "Hello Everybody";
+    }
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Olaa %s!", name);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RentalVehiclesTestApplication.class, args);
 	}
