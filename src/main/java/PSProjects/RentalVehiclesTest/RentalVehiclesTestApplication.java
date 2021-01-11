f79919caebd828424b244b606f6c86e69024c79e
Geremias Corrêa <gere.c@hotmail.com>
2021-01-11 09:00:57 -0300

    Some changes to make this thing works.

--- src/main/java/PSProjects/RentalVehiclesTest/RentalVehiclesTestApplication.java
+++ src/main/java/PSProjects/RentalVehiclesTest/RentalVehiclesTestApplication.java
@@ -2,6 +2,7 @@ package PSProjects.RentalVehiclesTest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @SpringBootApplication
-public class RentalVehiclesTestApplication {
+public class RentalVehiclesTestApplication extends SpringBootServletInitializer {
 
 	@RequestMapping("/")
     public String home(){
