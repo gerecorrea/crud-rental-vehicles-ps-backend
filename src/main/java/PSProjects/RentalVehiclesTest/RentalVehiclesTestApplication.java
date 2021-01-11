2b15ecb7187493cffa3f98ffa823901729ae6eeb
Geremias Corrêa <gere.c@hotmail.com>
2021-01-11 08:42:32 -0300

    First commit to test on heroku

---
+++ src/main/java/PSProjects/RentalVehiclesTest/RentalVehiclesTestApplication.java
@@ -0,0 +1,13 @@
+package PSProjects.RentalVehiclesTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalVehiclesTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalVehiclesTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PSProjects/RentalVehiclesTest/RentalVehiclesTestApplicationTests.java
@@ -0,0 +1,13 @@
+package PSProjects.RentalVehiclesTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalVehiclesTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
