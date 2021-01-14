81d448951d1c21e049a7291fd68d063f5c7d3696
Geremias Corrêa <gere.c@hotmail.com>
2021-01-14 11:16:51 -0300

    First backend part is ok.

--- src/main/java/PSProjects/RentalVehiclesTest/controller/VehicleController.java
+++ src/main/java/PSProjects/RentalVehiclesTest/controller/VehicleController.java
@@ -27,13 +27,39 @@ public class VehicleController {
 		v1.setId(1);
 		v1.setName("Maverick");
 		v1.setBrand("Ford");
+		v1.setDescription("Motor 3.8, alto consumo, baixo conforto, estilo");
+		v1.setYear(1973);
+		v1.setLicense_plate("MHG0001");
+		v1.setColor("Amarelo");
+		v1.setFipe(45000);
+		v1.setInsurance(2800);
+		v1.setPhoto("randomlink");
 		
 		var v2 = new Vehicle();
 		v2.setId(2);
 		v2.setName("Edge");
 		v2.setBrand("Ford");
+		v2.setDescription("Motor 1.6, médio consumo, alto conforto, muita manutenção");
+		v2.setYear(2013);
+		v2.setLicense_plate("MHG0002");
+		v2.setColor("Prata");
+		v2.setFipe(52000);
+		v2.setInsurance(1900);
+		v2.setPhoto("randomlink");
 		
-		return Arrays.asList(v1, v2);
+		var v3 = new Vehicle();
+		v3.setId(3);
+		v3.setName("Corsa");
+		v3.setBrand("Chevrolet");
+		v2.setDescription("Motor 1.0, baixoconsumo, baixo conforto, sem estilo.");
+		v2.setYear(2003);
+		v2.setLicense_plate("MHG0003");
+		v2.setColor("Azul");
+		v2.setFipe(6500);
+		v2.setInsurance(1300);
+		v2.setPhoto("randomlink");
+		
+		return Arrays.asList(v1, v2, v3);
 	}
 
 }
